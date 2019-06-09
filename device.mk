5e846ef68016193c5ce046de310b5184f80d32fe
GuaiYiHu <guaiyihu@foxmail.com>
2019-06-09 19:34:09 +0800

    wt88047: Switch to prebuilt sensor hal
    Change-Id: I656893e8d246a88f39c7e0a2223ce24aea2f703d

--- device.mk
+++ device.mk
@@ -109,13 +109,11 @@ PRODUCT_PACKAGES += \
 # Sensor HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    calmodule.cfg \
-    libcalmodule_common \
-    sensors.msm8916 \
-    sensors.wt88047
+    sensors.msm8916
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf \
+    $(LOCAL_PATH)/configs/calmodule.cfg:system/vendor/etc/calmodule.cfg
 
 # System Properties
 $(call inherit-product, device/wingtech/wt88047/system_prop.mk)
